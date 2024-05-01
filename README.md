fa177a48dd0158ea214006c1e990c79b6aebaf8a
Fraser Morrison <frazemor@hotmail.com>
2024-05-01 16:54:15 -0400

    Creating multiple concurrent and shareable games

--- README.md
+++ README.md
@@ -7,9 +7,10 @@ Built as a learning exercise in scaling a Go application from single client to m
 Goals:
 
 - Learn Go testing and implement some testing.
-- 1v1 on different clients.
-- Multiple concurrent games.
-- Allow creation of games and sharing with link.
+- 1v1 on different clients (distinguish P1 and P2).
+- ~~Multiple concurrent games.~~
+- ~~Allow creation of games and sharing with link.~~
+- Deleting inactive games (There is a memory leak lol)
 - Simulation testing ¯\\\_(ツ)\_/¯
 - Set up tailwind bundling
 - Make it pretty
--- squares.go
+++ squares.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"crypto/rand"
 	"fmt"
 	"html/template"
 	"net/http"
 	"strconv"
+	"sync"
 )
 
 type Game struct {
+	Id            string
 	Board         Board
 	CurrentPlayer Tile
 }
@@ -48,14 +51,55 @@ func (g *Game) AddTile(col int, player Tile) (Tile, error) {
 	return Empty, nil
 }
 
+func NewGame(id string) Game {
+	return Game{
+		Id:            id,
+		Board:         NewBoard(),
+		CurrentPlayer: Red,
+	}
+}
+
 type PlayerSession struct {
 	Player Tile
 	Board  Board
 	IsTurn bool
 }
 
+func new_game_id() (string, error) {
+	b := make([]byte, 4)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%x", b), nil
+}
+
+func UniqueGameId(games *sync.Map, attempts uint) (string, error) {
+
+	var err error = nil
+	var uuid string
+
+	var i uint
+	for i = 0; i < attempts; i++ {
+		// Try 10 times to generate a unique game ID
+		// If it fails 10 times, return an error
+		uuid, err = new_game_id()
+
+		if err != nil {
+			continue
+		}
+
+		if _, ok := games.Load(uuid); !ok {
+			break
+		}
+
+		err = fmt.Errorf("Game ID already exists")
+	}
+	return uuid, err
+}
+
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println("Initializing server...")
 
 	templates, err := template.ParseFiles("templates/index.html", "templates/game.html")
 
@@ -64,14 +108,66 @@ func main() {
 		return
 	}
 
-	gameState := Game{
-		Board:         NewBoard(),
-		CurrentPlayer: Red,
-	}
+	// TODO: Figure out how to clean up inactive games
+
+	// Investigate how go handles map size (auto downsizing?)
+
+	var games sync.Map
 
 	var mux = http.NewServeMux()
 
-	mux.HandleFunc("/add", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/new", func(w http.ResponseWriter, r *http.Request) {
+
+		// Try to generate a unique game ID
+		uuid, err := UniqueGameId(&games, 10)
+
+		if err != nil {
+			http.Error(w, "Could not generate unique game ID after 10 attempts", http.StatusInternalServerError)
+			return
+		}
+
+		gameState := NewGame(uuid)
+
+		games.Store(uuid, gameState)
+
+		http.Header.Add(w.Header(), "HX-Push-Url", "/game/"+uuid)
+
+		err = templates.ExecuteTemplate(w, "board", gameState)
+		if err != nil {
+			fmt.Println(err)
+		}
+	})
+
+	mux.HandleFunc("/game/{id}", func(w http.ResponseWriter, r *http.Request) {
+		idString := r.PathValue("id")
+		v, ok := games.Load(idString)
+
+		if !ok {
+			// TODO: Send 404 page
+			http.Error(w, "Game not found", http.StatusNotFound)
+			return
+		}
+
+		gameState := v.(Game)
+
+		err = templates.ExecuteTemplate(w, "board", gameState)
+
+	})
+
+	mux.HandleFunc("/game/{id}/add", func(w http.ResponseWriter, r *http.Request) {
+
+		idString := r.PathValue("id")
+
+		v, ok := games.Load(idString)
+
+		if !ok {
+			// TODO: Send 404 page
+			http.Error(w, "Game not found", http.StatusNotFound)
+			return
+		}
+
+		gameState := v.(Game)
+
 		params := r.URL.Query()
 		column := params.Get("c")
 		col, err := strconv.Atoi(column)
@@ -114,6 +210,9 @@ func main() {
 			gameState.Reset()
 		}
 
+		// Save game state
+		games.Store(idString, gameState)
+
 		// Send new board
 		err = templates.ExecuteTemplate(w, "board", gameState)
 
@@ -124,13 +223,20 @@ func main() {
 	})
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		err = templates.ExecuteTemplate(w, "index", gameState)
+		err = templates.ExecuteTemplate(w, "index", nil)
 		if err != nil {
 			fmt.Println(err)
 		}
 	})
 
-	http.ListenAndServe(":8080", mux)
 	fmt.Println("Listening on port 8080")
-	fmt.Println("http://localhost:8080/")
+	fmt.Println("Serving at http://localhost:8080/")
+
+	err = http.ListenAndServe(":8080", mux)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 }
